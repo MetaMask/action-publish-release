6430a86891799db614de248f8821dcfd0aca30a4
Shane <jonas.shane@gmail.com>
2021-04-07 11:28:41 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,6 @@ export PREFIX=${1}
 export NEW_VERSION="${GITHUB_HEAD_REF#$PREFIX}"
 export RELEASE_BODY="$(awk -v version="${NEW_VERSION}" -f scripts/show-changelog.awk CHANGELOG.md)"
 
-yarn install
-yarn build
-pushd build
-
 hub release create \
     --message "Version ${NEW_VERSION}" \
     --message "${RELEASE_BODY}" \
