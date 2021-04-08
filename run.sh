20c2d29ea67d33f2ec288c4ec96934c833c85f09
shanejonas <jonas.shane@gmail.com>
2021-04-08 12:52:40 -0700

    fix: add path as arg to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export PREFIX=${1}
 export NEW_VERSION="${GITHUB_HEAD_REF#$PREFIX}"
-export RELEASE_BODY="$(awk -v version="${NEW_VERSION}" -f scripts/show-changelog.awk CHANGELOG.md)"
+export RELEASE_BODY="$(awk -v version="${NEW_VERSION}" -f ${2}/scripts/show-changelog.awk CHANGELOG.md)"
 
 hub release create \
     --message "Version ${NEW_VERSION}" \
