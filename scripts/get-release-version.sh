5c49603fdd0c765932d074b69c52152a732df1e3
Erik Marks <rekmarks@protonmail.com>
2021-05-28 10:40:49 -0700

    Change release branch prefix from 'release-v' to 'release/'

--- scripts/get-release-version.sh
+++ scripts/get-release-version.sh
@@ -15,7 +15,7 @@ fi
 # GITHUB_HEAD_REF is the name of the branch of the closed release PR, per:
 # https://docs.github.com/en/actions/reference/environment-variables
 # The following expression strips the expected branch prefix from the branch
-# name and assigns it to a RELEASE_VERSION. For example, "release-v1.0.0"
+# name and assigns it to a RELEASE_VERSION. For example, "release/1.0.0"
 # becomes "1.0.0".
 RELEASE_VERSION="${GITHUB_HEAD_REF#$RELEASE_BRANCH_PREFIX}"
 
