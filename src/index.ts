4a4ce995f211de2bbfffbe6ceb1e599f4d380104
rekmarks <rekmarks@protonmail.com>
2021-06-27 14:31:14 -0700

    Add debug logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { setFailed as setActionToFailed } from '@actions/core';
+import {
+  error as logError,
+  setFailed as setActionToFailed,
+} from '@actions/core';
 import { getReleaseNotes } from './getReleaseNotes';
 
 getReleaseNotes().catch((error) => {
+  logError(error?.stack || 'The error has no stack.');
   setActionToFailed(error);
 });
