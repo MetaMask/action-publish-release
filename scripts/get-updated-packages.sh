a89e67af07e1b2f539d9e30af8fdaca6fa98c41c
Ricky Miller <ricky.miller@gmail.com>
2022-08-24 11:25:19 -0400

    add get-updated-packages

---
+++ scripts/get-updated-packages.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# string of packages to publish
+toPublish="{\"packages\":["
+# store initial length of toPublish
+len="${#toPublish}"
+
+for dir in packages/*
+do
+  MANIFEST="$dir/package.json"
+  PRIVATE=$(jq .private "$MANIFEST")
+  if [[ "$PRIVATE" != "true" ]]; then
+    NAME=$(jq --raw-output .name "$MANIFEST")
+    LATEST_PACKAGE_VERSION=$(npm view "$NAME" version --workspaces=false || echo "")
+    CURRENT_PACKAGE_VERSION=$(jq --raw-output .version "$MANIFEST")
+
+    if [ "$LATEST_PACKAGE_VERSION" != "$CURRENT_PACKAGE_VERSION" ]; then
+      toPublish+="\"$NAME\","
+    fi
+  fi
+done
+
+# if the length of toPublish is greater than the initial length
+# trim off the last char (,)
+if [[ "${#toPublish}" -gt "$len" ]]; then
+  toPublish=${toPublish::-1}
+fi
+
+UPDATED_PACKAGES="$toPublish]}"
+
+# echo "$PACKAGES"
+echo "::set-output name=UPDATED_PACKAGES::$UPDATED_PACKAGES"
