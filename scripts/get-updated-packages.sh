5ecee7bedf90cf34d3d0db46ab88f12cce62a65a
Ricky Miller <ricky.miller@gmail.com>
2022-09-22 12:18:08 -0400

    call jq once here

--- scripts/get-updated-packages.sh
+++ scripts/get-updated-packages.sh
@@ -29,12 +29,9 @@ workspaces=$(yarn workspaces list --verbose --json)
 
 while read -r location name; do
   MANIFEST="$location/package.json"
-  PRIVATE=$(jq .private "$MANIFEST")
+  read PRIVATE NAME CURRENT_PACKAGE_VERSION < <(echo $(jq --raw-output '.private, .name, .version' "$MANIFEST"))
   if [[ "$PRIVATE" != "true" ]]; then
-    NAME=$(jq --raw-output .name "$MANIFEST")
     LATEST_PACKAGE_VERSION=$(npm view "$NAME" version --workspaces=false || echo "")
-    CURRENT_PACKAGE_VERSION=$(jq --raw-output .version "$MANIFEST")
-
     if [ "$LATEST_PACKAGE_VERSION" != "$CURRENT_PACKAGE_VERSION" ]; then
       toPublish+="\"$NAME\":{\"name\":"\"$NAME\"",\"path\":"\"$location\"",\"version\":"\"$CURRENT_PACKAGE_VERSION"\"},"
     fi
