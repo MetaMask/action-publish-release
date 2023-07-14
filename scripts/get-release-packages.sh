7561460fd829ef1fcaadab6b3807307e259adae9
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-07-14 12:57:48 +0200

    Add `npm-tag` input to use when checking latest version

--- scripts/get-release-packages.sh
+++ scripts/get-release-packages.sh
@@ -5,6 +5,8 @@ set -e
 set -u
 set -o pipefail
 
+tag="${1:-latest}"
+
 # JSON string of packages to publish
 # shape is as follows:
 # {
@@ -31,7 +33,7 @@ while read -r location name; do
   MANIFEST="$location/package.json"
   read -r PRIVATE CURRENT_PACKAGE_VERSION < <(jq --raw-output '.private, .version' "$MANIFEST" | xargs)
   if [[ "$PRIVATE" != "true" ]]; then
-    LATEST_PACKAGE_VERSION=$(npm view "$name" version --workspaces=false || echo "")
+    LATEST_PACKAGE_VERSION=$(yarn npm info "$name" --json --fields dist-tags | jq --raw-output --arg tag "$tag" '."dist-tags"[$tag]' || echo "")
     if [ "$LATEST_PACKAGE_VERSION" != "$CURRENT_PACKAGE_VERSION" ]; then
       toPublish+="\"$name\":{\"name\":"\"$name\"",\"path\":"\"$location\"",\"version\":"\"$CURRENT_PACKAGE_VERSION"\"},"
     fi
